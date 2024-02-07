a26b768a03da06cede536285f773b5801055e833
pablomaruottolo <pablogabriel.maruottolo@cieep.com>
2024-02-07 12:31:14 +0100

    Initial Commit

---
+++ Ejercicio2ExamenE2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ejercicio2ExamenE2
+//
+//  Created by Pablo Maruottolo on 07/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
