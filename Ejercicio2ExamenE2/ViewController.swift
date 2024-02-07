81e73c8959c69e2aa2f864f12aae6eb81dcc758e
pablomaruottolo <pablogabriel.maruottolo@cieep.com>
2024-02-07 13:13:10 +0100

    ExamenEvaluacion2Ejercicio2

--- Ejercicio2ExamenE2/ViewController.swift
+++ Ejercicio2ExamenE2/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var txtNombre: UIView!
+    @IBOutlet weak var txtTelefono: UITextField!
+    @IBOutlet weak var txtPersonaContacto: UITextField!
+    @IBOutlet weak var txtEstado: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnCrear(_ sender: Any) {
+        
+    }
+    
+    @IBAction func btnModificar(_ sender: Any) {
+        
+    }
+    @IBAction func btnEliminar(_ sender: Any) {
+        
+    }
 }
 
---
+++ Ejercicio2ExamenE2/modelos/Empresa.swift
@@ -0,0 +1,17 @@
+//
+//  Empresa.swift
+//  Ejercicio2ExamenE2
+//
+//  Created by Pablo Maruottolo on 07/02/2024.
+//
+
+import Foundation
+
+struct Empresa{
+    let nombre:String
+    let telefono:String
+    let personaContacto:String
+    let estadoContrato:String
+    
+}
+
